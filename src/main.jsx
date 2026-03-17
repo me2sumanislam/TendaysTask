9efeac14e0bb60fb441866bed41570a6a223e3b9
me2sumanislam <me2sumanislam247@gmail.com>
2026-03-17 11:56:04 +0600

    add react routeing

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// react routing
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World dsgdhdf</div>,
+  },
+]);
+
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={router} />,
   </StrictMode>,
 )
