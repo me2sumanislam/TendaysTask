643e4ee0d3d04c0470be87a3f7a6c70303cde847
me2sumanislam <me2sumanislam247@gmail.com>
2026-03-17 17:34:24 +0600

    show json data on card

--- src/Pages/Book/Book.jsx
+++ src/Pages/Book/Book.jsx
@@ -1,12 +1,28 @@
  import React, {  } from 'react'
  
  const Book = ({singleBook}) => {
- console.log(singleBook);
+//  console.log(singleBook);
+ 
     
    return (
-     <div>
-       
-     </div>
+     <div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src={singleBook.image}
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      {singleBook.bookName}
+      <div className="badge badge-secondary">{singleBook.author}</div>
+    </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">Fashion</div>
+      <div className="badge badge-outline">Products</div>
+    </div>
+  </div>
+</div>
    )
  }
  
--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -21,12 +21,14 @@ import Book from '../Book/Book'
 
    return (
      <div>
-       <h2>ami books</h2>
       
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+             
         {
             data.map((singleBook)=><Book key={singleBook.bookId} singleBook={singleBook}></Book>)
         }
  
+        </div>
      </div>
    )
  }
